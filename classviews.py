6614d5c199ad1afbdc7aabc85072d67689c6d34e
Bhavishya <bhavvishyya@gmail.com>
2017-02-27 00:07:22 +0530

    Additional views using generic classes

---
+++ classviews.py
@@ -0,0 +1,20 @@
+from django.views.generic import ListView
+from django.views.generic import DetailView
+from App.forms import *
+
+
+class catelogueListView(ListView):
+    model=Catelogue
+    def get_context_data(self, **kwargs):
+        context = super(catelogueListView, self).get_context_data(**kwargs)
+        return context
+
+class catelogueDetailView(DetailView):
+    model=Catelogue
+    context_object_name = "cat_detail"
+    def get_object(self, queryset=None):
+        name=self.kwargs.get('name')
+        if name:
+            return Catelogue.objects.get(name=name)
+        else:
+            return Catelogue.objects.get(pk=self.kwargs["pk"])
